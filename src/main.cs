44a3bf2adbf61ab55b38bcf0aa7726c7c2e253f1
Jason Hitching <jasonhitching1994@gmail.com>
2026-05-28 20:54:22 +0100

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -5,7 +5,10 @@ class Program
         while (true)
         {
             Console.Write("$ ");
+
             var command = Console.ReadLine();
+            if (command is "exit") break;
+            
             Console.WriteLine($"{command}: command not found");
         }
     }
