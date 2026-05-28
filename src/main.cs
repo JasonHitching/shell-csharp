890783ab2fcff0434eca50237011a5a5bba2264c
Jason Hitching <jasonhitching1994@gmail.com>
2026-05-28 21:02:10 +0100

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -7,8 +7,12 @@ class Program
             Console.Write("$ ");
 
             var fullInput = Console.ReadLine();
-            var command = string.Split(fullInput).First();
-            
+
+            if (fullInput is not null)
+            {
+                var command = string.Split(fullInput).First();
+            }
+
             switch (command) {
               case "exit":
                 break;
