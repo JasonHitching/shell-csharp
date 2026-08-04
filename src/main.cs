7bf11ac2a1c4c36775f8a8ed787de874a62342ca
Jason Hitching <jasonhitching1994@gmail.com>
2026-08-04 18:28:24 +0100

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -2,14 +2,13 @@ using System.ComponentModel.Design;
 
 class Program
 {
+  static readonly List<string> builtins = ["echo", "exit", "type"];
+
   static void Main()
   {
     while (true)
     {
       Console.Write("$ ");
-
-      var builtins = new List<string> { "echo", "exit", "type" };
-
       var command = Console.ReadLine();
       var splitCommand = command.Split(' ', 2);
 
@@ -22,7 +21,8 @@ class Program
           break;
         case "type":
           string secondCommand = splitCommand[1];
-          if (builtins.Contains(secondCommand)) // recognised supported command
+
+          if (CheckForBuiltin(secondCommand)) // recognised supported command
           {
             Console.WriteLine($"{secondCommand} is a shell builtin");
           }
@@ -30,12 +30,40 @@ class Program
           {
             Console.WriteLine($"{secondCommand}: not found");
           }
+
           break;
         default:
           Console.WriteLine($"{command}: command not found");
           break;
       }
       ;
+    }
+  }
+
+  static bool CheckForBuiltin(string secondCommand)
+  {
+    if (builtins.Contains(secondCommand)) // recognised supported command
+    {
+      return true;
+    }
+
+    return false;
+  }
+
+  static bool CheckForPath(string secondCommand)
+  {
+    var environmentPaths = Environment.GetEnvironmentVariables();
+
+    foreach (var path in environmentPaths)
+    {
+      if (path is null) continue;
+
+      var directoryFiles = Directory.EnumerateFiles(path.ToString());
+
+      foreach (var file in directoryFiles)
+      {
+        Console.WriteLine(file);
+      }
     }
   }
 }
